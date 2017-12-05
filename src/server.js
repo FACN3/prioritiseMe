71e8f6a54dc919944280db7e1a80a45af9d0eb6e
SamiSha99 <sami.shakkour@hotmail.com>
2017-12-05 17:58:46 +0200

    Finaly Product design for the html + css! (mobile only)

--- src/server.js
+++ src/server.js
@@ -6,5 +6,5 @@ const host = process.env.HOST || 'localhost';
 const server = http.createServer(router);
 
 server.listen(port, () => {
-  console.log(`The Port is open on ${port} go to localhost:${port}`);
+  console.log(`The Port is open on ${port} head to localhost:${port} to access the site`);
 });
