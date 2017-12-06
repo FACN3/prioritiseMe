a55e24870c47482648bc9472ef42601272746f39
ionush <neilasjoseph@gmail.com>
2017-12-06 14:17:10 +0200

    added get and post data on db side
    #15

---
+++ src/queries/getData.js
@@ -0,0 +1,14 @@
+const connect = require('../database/db_connection');
+
+const getData = cb => {
+  connect.query(
+    'select tasks.id,tasks.description,tasks.priority,tasks.time_started, tasks.time_finished, users.name from tasks inner join users on users.id=tasks.user_id',
+    (err, res) => {
+      if (err) cb(err);
+      const result = res.rows;
+      cb(null, result);
+    }
+  );
+};
+
+module.exports = getData;
